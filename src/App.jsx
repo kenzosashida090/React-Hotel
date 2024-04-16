743bbc86f012fd4b9b76443d4aca5f3f1bbc6d1a
kenzosashida090 <kenzosashida1@gmail.com>
2024-04-16 13:21:01 -0600

    Add: Global Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
 
 import styled from 'styled-components' // styled component library
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
 
 const H1 = styled.h1` // styled. html element
   font-size:30px;
   font-weight: 600;
 `;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2 rem 1.6rem;
-  font-weight: 500;
-  border:none;
-  border-radius: 7px;
-  background-color: purple;
-  color:white;
-  margin:20px;
-`
-const Input = styled.input `
-  border:1px solid #ddd;
-  border-radius:8px;
-  padding:0.8rem 1.2rem;
-
-`
 
 const StyledApp = styled.main`
   background-color:orange;
@@ -31,12 +17,16 @@ const StyledApp = styled.main`
 function App() {
 
   return (
-    <StyledApp>
+    <>
+    
+      <GlobalStyles/> 
+      <StyledApp>
       <H1>Gentlemen&apos;s Hotel Club</H1>
       <Button>Check in</Button>
       <Input type='number' placeholder='Number of guests' />
       <Input  type='number' placeholder='Number of guests' />
-    </StyledApp>
+      </StyledApp>
+    </>
   )
 }
 
