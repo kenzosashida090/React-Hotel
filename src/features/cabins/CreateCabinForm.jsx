1738a31cefb9a64e268452d12f637a8bfc1c325d
kenzosashida090 <kenzosashida1@gmail.com>
2024-04-25 13:09:36 -0600

    Add: React hook Form & Form validations

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -9,32 +9,33 @@ import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createCabin } from "../../services/apiCabins";
 import toast from "react-hot-toast";
+import FormRow from "../../ui/FormRow";
 
-const FormRow = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
+const SFormRow = styled.div`
+display: grid;
+align-items: center;
+grid-template-columns: 24rem 1fr 1.2fr;
+gap: 2.4rem;
 
-  padding: 1.2rem 0;
+padding: 1.2rem 0;
 
-  &:first-child {
-    padding-top: 0;
-  }
+&:first-child {
+  padding-top: 0;
+}
 
-  &:last-child {
-    padding-bottom: 0;
-  }
+&:last-child {
+  padding-bottom: 0;
+}
 
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
+&:not(:last-child) {
+  border-bottom: 1px solid var(--color-grey-100);
+}
 
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
+&:has(button) {
+  display: flex;
+  justify-content: flex-end;
+  gap: 1.2rem;
+}
 `;
 
 const Label = styled.label`
@@ -47,7 +48,8 @@ const Error = styled.span`
 `;
 
 function CreateCabinForm() {
-  const {register, handleSubmit,reset} = useForm(); // react-hook-form
+  const {register, handleSubmit,reset, getValues, formState} = useForm(); // react-hook-form
+  const {errors} = formState // get the errors from the form
   const queryClient = useQueryClient(); // Get the isntance of the queryClient to re render whenever whe create a new cabin 
   const {mutate,isLoading} = useMutation({ //Create a mutation to create a new cabin
     mutationFn: createCabin,
@@ -62,46 +64,55 @@ function CreateCabinForm() {
 
     mutate(data)
   }
+  function onError(errors) {
+
+  }
   //1) The first step that we follow is to register all the form inputs
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <FormRow>
-        <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" {...register("name")} />
+    <Form onSubmit={handleSubmit(onSubmit,onError )}> {/** we can pass two fn to the handleSubmit, the onSubmit handler  and the handleError */}
+      <FormRow label="Cabin name" error={errors?.name?.message}>
+        <Input type="text" id="name" {...register("name",
+          {
+            required:"This field is required", 
+            min:{ value:1, message:"Capacity should be at least one"}
+          } //Validating all the inputs with the hook form
+        )} />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="max_capacity">Maximum capacity</Label>
-        <Input type="number" id="max_capacity" {...register("max_capacity")} />
+      <FormRow label="Max capacity" error={errors?.max_capacity?.message}>
+        <Input type="number" id="max_capacity" {...register("max_capacity",{required:"This field is required"})}  />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="regular_price">Regular price</Label>
-        <Input type="number" id="regular_price" {...register("regular_price")} />
+      <FormRow label="Regular price" error={errors?.regular_price?.message}>
+        <Input type="number" id="regular_price" {...register("regular_price",{required:"This field is required"})}  />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="discount">Discount</Label>
-        <Input type="number" id="discount" defaultValue={0} {...register("discount")}  />
+      <FormRow label="discount" error={errors?.discount?.message}>
+        <Input type="number" id="discount" defaultValue={0} {...register("discount",
+          {
+            required:"This field is required",
+            validate: (value) => value <= getValues().regular_price || "Discount should be less than the regular price", 
+            /** WE can write our own validate fns, if the condition isnt true will trigger the message error */
+          
+          })}   />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="description">Description for website</Label>
-        <Textarea type="number" id="description" defaultValue="" {...register("number")} />
+      <FormRow label="description" error={errors?.description?.message}>
+        <Textarea type="number" id="description" defaultValue="" {...register("description",{required:"This field is required"})}  />
       </FormRow>
 
-      <FormRow>
+      <SFormRow>
         <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*"  />
-      </FormRow>
+        <FileInput id="image" accept="image/*" type="file" {...register("image",{required:"This field is required"})}  />
+      </SFormRow>
 
-      <FormRow>
+      <SFormRow>
         {/* type is an HTML attribute! */}
         <Button variation="secondary" type="reset">{/*type="reset"  resets the form but it will not work as a suibmit bnutton */}
           Cancel
         </Button>
         <Button disabled={isLoading}>Add cabin</Button>
-      </FormRow>
+      </SFormRow>
     </Form>
   );
 }
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -6,12 +6,14 @@ import styled from "styled-components"
 const Main = styled.main`
     background-color:var(--color-grey-50);;
     padding:4rem 4.8rem 6.4rem;
+    overflow: scroll;
 `
 const StyledAppLayout = styled.div`
     display:grid;
     height:100vh;
     grid-template-columns: 26rem 1fr;
     grid-template-rows: auto 1fr;
+    overflow-y:hidden;
 `
 const Container = styled.div`
   max-width:120rem;
