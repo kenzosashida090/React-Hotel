2edda48261532b53d3625c1de2c2b2b8c96a3c77
kenzosashida090 <kenzosashida1@gmail.com>
2024-05-03 12:05:59 -0600

    Add: Render Pattern Prop

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -88,7 +88,12 @@ function Row({children}){
     </StyledRow>
   )
 }
-function Body({children}){}
+function Body({data,render}){
+  if(!data.length) return <Empty>No data Show at the moment</Empty>
+  return <StyledBody>
+    {data.map(render)} {/**Render PROP pattern */}
+  </StyledBody>
+}
 Table.Header = Header
 Table.Row = Row
 Table.Body = Body
