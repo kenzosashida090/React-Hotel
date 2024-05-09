2514791766063d462d284c02df7ddbd1fd2cc2db
kenzosashida090 <kenzosashida1@gmail.com>
2024-05-09 15:07:53 -0600

    Add: Pagination

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -4,6 +4,7 @@ import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty";
 import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
+import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
   const {isLoading, bookings}  = useBookings()
@@ -28,6 +29,9 @@ function BookingTable() {
             <BookingRow key={booking.booking_id} booking={booking} />
           )}
         />
+        <Table.Footer>
+          <Pagination count={23}/>
+        </Table.Footer>
       </Table>
     </Menus>
   );
