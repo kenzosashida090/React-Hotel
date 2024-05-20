c8d2ffedc6d153132634ed56190d67b264f9ab89
kenzosashida090 <kenzosashida1@gmail.com>
2024-05-20 10:40:29 -0600

    Add: Auth Login User

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -2,14 +2,23 @@ import { useState } from "react";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
+import SpinnerMini from "../../ui/SpinnerMini";
 import FormRowVertical from "../../ui/FormRowVertical";
+import { useLogin } from "./useLogin";
 
 function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const {login,isLoading} = useLogin()
 
-  function handleSubmit() {}
+  function handleSubmit(e) {
+     e.preventDefault()
+     if(!email || !password) return
 
+     login({email,password})
+
+  }
+  
   return (
     <Form onSubmit={handleSubmit}>
       <FormRowVertical label="Email address">
@@ -20,6 +29,7 @@ function LoginForm() {
           autoComplete="username"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isLoading}
         />
       </FormRowVertical>
       <FormRowVertical label="Password">
@@ -29,10 +39,14 @@ function LoginForm() {
           autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isLoading}
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large">Login</Button>
+        <Button disabled={isLoading} size="large">
+       {!isLoading ? "Log in" : <SpinnerMini/> }
+        
+        </Button>
       </FormRowVertical>
     </Form>
   );
